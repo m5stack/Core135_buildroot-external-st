6d7b28c2bb4633595bb9e350ba9a6ae2e8e6296f
dianjixz <dianjixz@m5stack.com>
2024-06-26 17:09:41 +0800

    [update] Change fb_ili9341_m5 to fb_ili9342c, Add axp2101 driver, Add drm_ili9342c.

--- board/m5stack/overlay/etc/profile.d/lcd_fb_num.sh
+++ board/m5stack/overlay/etc/profile.d/lcd_fb_num.sh
@@ -1,2 +1,2 @@
-export LCD_FRAMEBUFFER=`cat /proc/fb | grep fb_ili9341 | awk '{print "/dev/fb"$1}'`
+export LCD_FRAMEBUFFER=`cat /proc/fb | grep fb_ili9342c | awk '{print "/dev/fb"$1}'`
 export HDMI_FRAMEBUFFER=`cat /proc/fb | grep stmdrmfb | awk '{print "/dev/fb"$1}'`
