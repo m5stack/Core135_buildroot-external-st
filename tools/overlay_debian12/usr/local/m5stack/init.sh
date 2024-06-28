3bfad366e4192f153718c08aaa8d6b0b3543aa5f
dianjixz <dianjixz@m5stack.com>
2024-06-28 18:10:39 +0800

    [update] The method of loading the ili9342c kernel module

--- tools/overlay_debian12/usr/local/m5stack/init.sh
+++ tools/overlay_debian12/usr/local/m5stack/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+/sbin/modprobe fb_ili9342c
 . /usr/local/m5stack/bashrc
 printf "q\r\n" | fbv /usr/local/m5stack/logo.jpg 2>&1 > /dev/null &
 /usr/local/m5stack/lt8618sxb_mcu_config 2>&1 > /dev/null &
